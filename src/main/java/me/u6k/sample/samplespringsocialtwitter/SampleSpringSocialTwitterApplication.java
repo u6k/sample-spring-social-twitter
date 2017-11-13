b3d7cf9b65826882243269eea460fda2b9259e91
u6k <u6k.apps@gmail.com>
2017-11-13 12:02:06 +0900

    Spring Initializrでプロジェクトを作成しました。 #1

---
+++ src/main/java/me/u6k/sample/samplespringsocialtwitter/SampleSpringSocialTwitterApplication.java
@@ -0,0 +1,12 @@
+package me.u6k.sample.samplespringsocialtwitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringSocialTwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringSocialTwitterApplication.class, args);
+	}
+}
