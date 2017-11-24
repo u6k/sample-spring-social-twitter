33c86f03b0ef1e6d314eab17450625ca2a01eb0e
u6k <u6k.apps@gmail.com>
2017-11-24 18:01:51 +0900

    Javadoc、ライセンス表記を削除 #3

--- src/main/java/me/u6k/sample/sample_spring_social_twitter/social/facebook/FacebookHandlerInterceptor.java
+++ src/main/java/me/u6k/sample/sample_spring_social_twitter/social/facebook/FacebookHandlerInterceptor.java
@@ -12,17 +12,11 @@ import org.springframework.web.servlet.ModelAndView;
 public class FacebookHandlerInterceptor implements HandlerInterceptor {
 
     @Override
-    public void afterCompletion(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, Exception arg3)
-        throws Exception {
-        // TODO Auto-generated method stub
-
+    public void afterCompletion(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, Exception arg3) throws Exception {
     }
 
     @Override
-    public void postHandle(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, ModelAndView arg3)
-        throws Exception {
-        // TODO Auto-generated method stub
-
+    public void postHandle(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, ModelAndView arg3) throws Exception {
     }
 
     @Override
--- src/main/java/me/u6k/sample/sample_spring_social_twitter/social/facebook/PostToWallAfterConnectInterceptor.java
+++ src/main/java/me/u6k/sample/sample_spring_social_twitter/social/facebook/PostToWallAfterConnectInterceptor.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2014 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 package me.u6k.sample.sample_spring_social_twitter.social.facebook;
 
@@ -40,7 +25,6 @@ public class PostToWallAfterConnectInterceptor implements ConnectInterceptor<Fac
             try {
                 connection.updateStatus("I've connected with the Spring Social Showcase!");
             } catch (ApiException e) {
-                // Do nothing: No need to break down if the post-connect post can't be made.
             }
             request.removeAttribute(POST_TO_WALL_ATTRIBUTE, WebRequest.SCOPE_SESSION);
         }
