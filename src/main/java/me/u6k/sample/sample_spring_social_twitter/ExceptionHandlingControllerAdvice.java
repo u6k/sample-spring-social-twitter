5e094027073973f54d6007ffa8e809d3c99c3e9d
u6k <u6k.apps@gmail.com>
2017-11-24 17:12:41 +0900

    ベース・パッケージを変更 #3
    ベース・パッケージを"org.springframework.social.showcase"から"me.u6k.sample_spring_social_twitter"に変更しました。

---
+++ src/main/java/me/u6k/sample/sample_spring_social_twitter/ExceptionHandlingControllerAdvice.java
@@ -0,0 +1,17 @@
+package me.u6k.sample.sample_spring_social_twitter;
+
+import org.springframework.social.RateLimitExceededException;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionHandlingControllerAdvice {
+
+	@ExceptionHandler(RateLimitExceededException.class)
+	public String rateLimitExceeded(RateLimitExceededException e, Model model) {
+		model.addAttribute("providerId", e.getProviderId());
+		return "ratelimit";
+	}
+	
+}
---
+++ src/main/java/me/u6k/sample/sample_spring_social_twitter/config/Application.java
@@ -0,0 +1,25 @@
+package me.u6k.sample.sample_spring_social_twitter.config;
+
+import me.u6k.sample.sample_spring_social_twitter.signin.SimpleSignInAdapter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.social.connect.web.SignInAdapter;
+
+@ComponentScan(basePackages="me.u6k.sample.sample_spring_social_twitter")
+@EnableConfigurationProperties
+@EnableAutoConfiguration
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+	@Bean
+	public SignInAdapter signInAdapter() {
+		return new SimpleSignInAdapter(new HttpSessionRequestCache());
+	}
+
+}
